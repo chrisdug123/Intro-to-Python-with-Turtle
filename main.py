f4d1f25cd940a5f375956654b559e2ed8f5b3866
chrisdug123 <139854947+chrisdug123@users.noreply.github.com>
2024-07-24 17:25:33 +0000

    Creating a square

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-"""
-This is the Template Repl for Python with Turtle.
+# """
+# This is the Template Repl for Python with Turtle.
 
-Python with Turtle lets you make graphics easily in Python.
+# Python with Turtle lets you make graphics easily in Python.
 
-Check out the official docs here: https://docs.python.org/3/library/turtle.html
-"""
+# Check out the official docs here: https://docs.python.org/3/library/turtle.html
+# """
 
+# import turtle
+
+# # Fullscreen the canvas
+# screen = turtle.Screen()
+# screen.setup(1.0, 1.0)
+
+# # Begin!
+# t = turtle.Turtle()
+
+# for c in ['red', 'green', 'blue', 'yellow']:
+#   t.color(c)
+#   t.forward(75)
+#   t.left(90)
+
+# screen.mainloop()
+
+from turtle import *
 import turtle
 
-# Fullscreen the canvas
 screen = turtle.Screen()
 screen.setup(1.0, 1.0)
-
-# Begin!
 t = turtle.Turtle()
-
-for c in ['red', 'green', 'blue', 'yellow']:
-  t.color(c)
-  t.forward(75)
-  t.left(90)
+for i in range(4):
+  t.forward(50)
+  t.right(90)
 
 screen.mainloop()
